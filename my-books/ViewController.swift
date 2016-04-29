9cc3a8ca017dc4b28bda36bdb3705bc18f46725c
zephyz <zephyz.zhaar@gmail.com>
2016-04-30 00:48:48 +0200

    add project sources

---
+++ my-books/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-books
+//
+//  Created by zephyz on 30/04/16.
+//  Copyright © 2016 zephyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
