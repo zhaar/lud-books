18d729a5449f67287dce7eb78812188d833474df
zephyz <zephyz.zhaar@gmail.com>
2016-05-03 00:56:54 +0200

    add network button UI

--- my-books/Network.swift
+++ my-books/Network.swift
@@ -10,6 +10,24 @@ import Foundation
 import Alamofire
 import BrightFutures
 
+struct ServerInfo{
+    static let serverURL = "http://localhost"
+    static let serverPort = "5984"
+    static let dbPath = "_all_dbs"
+    static func serverPath() -> String {
+        return self.serverURL + ":" + self.serverPort + "/" + dbPath
+    }
+}
+
 func getBooks() -> [Book] {
-    
+    print("requesting")
+    Alamofire.request(.GET, ServerInfo.serverPath())
+        .responseJSON { response in
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            } else {
+                print("got \(response.response) instead")
+            }
+    }
+    return []
 }
--- my-books/ViewController.swift
+++ my-books/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func GetAllBooks(sender: UIButton) {
+        getBooks()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
